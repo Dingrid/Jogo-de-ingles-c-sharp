49b03b671f6c8b7f6ffa86c63e1c7c092d05b1f3
Dingrid <dingrisouzz@gmail.com>
2020-08-11 22:19:26 -0300

    Teste de usuario

--- ingres/ingres/Quest14.Designer.cs
+++ ingres/ingres/Quest14.Designer.cs
@@ -28,7 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnWell = new System.Windows.Forms.Button();
+            this.btnKnow = new System.Windows.Forms.Button();
+            this.btnHow = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(44, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 16);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "one of the simplest DoS tools.";
+            // 
+            // btnWell
+            // 
+            this.btnWell.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._144559;
+            this.btnWell.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnWell.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWell.ForeColor = System.Drawing.Color.Navy;
+            this.btnWell.Location = new System.Drawing.Point(88, 258);
+            this.btnWell.Name = "btnWell";
+            this.btnWell.Size = new System.Drawing.Size(75, 23);
+            this.btnWell.TabIndex = 30;
+            this.btnWell.Text = "well";
+            this.btnWell.UseVisualStyleBackColor = true;
+            // 
+            // btnKnow
+            // 
+            this.btnKnow.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._144559;
+            this.btnKnow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKnow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKnow.ForeColor = System.Drawing.Color.Navy;
+            this.btnKnow.Location = new System.Drawing.Point(88, 177);
+            this.btnKnow.Name = "btnKnow";
+            this.btnKnow.Size = new System.Drawing.Size(75, 23);
+            this.btnKnow.TabIndex = 29;
+            this.btnKnow.Text = "know";
+            this.btnKnow.UseVisualStyleBackColor = true;
+            // 
+            // btnHow
+            // 
+            this.btnHow.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._144559;
+            this.btnHow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHow.ForeColor = System.Drawing.Color.Navy;
+            this.btnHow.Location = new System.Drawing.Point(88, 100);
+            this.btnHow.Name = "btnHow";
+            this.btnHow.Size = new System.Drawing.Size(75, 23);
+            this.btnHow.TabIndex = 28;
+            this.btnHow.Text = "how";
+            this.btnHow.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(-1, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "LOIC is probably the most __-known and";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(2, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 23);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "CONHECIMENTOS GERAIS";
             // 
             // Quest14
             // 
@@ -36,14 +117,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(268, 283);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnWell);
+            this.Controls.Add(this.btnKnow);
+            this.Controls.Add(this.btnHow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Quest14";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quest14";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnWell;
+        private System.Windows.Forms.Button btnKnow;
+        private System.Windows.Forms.Button btnHow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
